4a9efa0e339c93ea3f2c82fa60656f45852a7263
Junhyun Shin <hl1sqi@gmail.com>
2025-04-21 13:09:43 +0900

    allow osc52 yank and paste

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -67,6 +67,37 @@
 
 -- You should make sure your terminal supports this
 -- vim.opt.termguicolors = true
+--
+
+-- [[Sync clipboard between OS and Neovim]]
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+if vim.env.TMUX ~= nil then
+  local copy = { "tmux", "load-buffer", "-w", "-" }
+  local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
+  vim.g.clipboard = {
+    name = "tmux",
+    copy = {
+      ["+"] = copy,
+      ["*"] = copy,
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+    cache_enabled = 0,
+  }
+end
 
 -- [[Language providers]]
 vim.g.python3_host_prog = vim.fn.expand("~/.pyenv/shims/python")
